60185d6270ad3a2932d276890ce74c8dbb9bfebb
zachakin <roak1317@colorado.edu>
2021-09-16 13:41:15 -0600

    solve Sum() and VectorPlusN() on ZachsBranch

--- functions_to_implement.cpp
+++ functions_to_implement.cpp
@@ -30,13 +30,24 @@ std::vector<bool> EvenMask(std::vector<int>);
 std::vector<bool> OddMask(std::vector<int>);
 
 // Sums all numbers in a vector and returns the resulting value
-int Sum(std::vector<int> nums);
+int Sum(std::vector<int> nums){
+    int sum =0;
+    for(int i=0;i<nums.size();i++){
+        sum+=nums[i];
+    }
+    return sum;
+}
 
 // Multiplies all numbers in a vector together and returns the resulting value
 int Product(std::vector<int> nums);
 
 // Adds an integer n to each element of a given vector
-std::vector<int> VectorPlusN(std::vector<int> v, int n);
+std::vector<int> VectorPlusN(std::vector<int> v, int n){
+    for(int i=0;i<v.size();i++){
+        v[i]+=n;
+    }
+    return v;
+}
 
 // Multiples an integer n with each element of a given vector
 std::vector<int> VectorTimesN(std::vector<int> v, int n);
